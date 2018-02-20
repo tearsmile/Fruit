3e0813548e30cbc873246e06c1d02e2cb665e235
CJ <635405779>
2018-02-20 22:40:45 +0800

    preference

--- app/src/main/java/com/bowl/fruit/preference/PreferenceDao.java
+++ app/src/main/java/com/bowl/fruit/preference/PreferenceDao.java
@@ -1,30 +1,8 @@
 package com.bowl.fruit.preference;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 
 public class PreferenceDao{
     private static final String SP_NAME = "SharedPreference_Fruit";
     private SharedPreferences preferences;
-
-    private static class singleton {
-        private static PreferenceDao instance = new PreferenceDao();
-    }
-
-    private PreferenceDao() {
-        preferences = ContextHolder.getAppContext().getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
-    }
-
-    public static PreferenceDao getInstance() {
-        return singleton.instance;
-    }
-
-    public String getString(String key, String defValue) {
-        return preferences.getString(key, defValue);
-    }
-
-    public boolean putString(String key, String value) {
-        return preferences.edit().putString(key, value).commit();
-    }
-
 }
