4820b1a6f7af943ea09c60acfd8f9e1ed3fbbbd8
CJ <635405779>
2018-02-20 22:39:27 +0800

    preference

--- app/src/main/java/com/bowl/fruit/preference/PreferenceDao.java
+++ app/src/main/java/com/bowl/fruit/preference/PreferenceDao.java
@@ -3,8 +3,7 @@ package com.bowl.fruit.preference;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class PreferenceDao {
-
+public class PreferenceDao{
     private static final String SP_NAME = "SharedPreference_Fruit";
     private SharedPreferences preferences;
 
@@ -28,39 +27,4 @@ public class PreferenceDao {
         return preferences.edit().putString(key, value).commit();
     }
 
-    public boolean getBoolean(String key, boolean defValue) {
-        return preferences.getBoolean(key, defValue);
-    }
-
-    public boolean putBoolean(String key, boolean value) {
-        return preferences.edit().putBoolean(key, value).commit();
-    }
-
-    public long getLong(String key, long defValue) {
-        return preferences.getLong(key, defValue);
-    }
-
-    public boolean putLong(String key, long value) {
-        return preferences.edit().putLong(key, value).commit();
-    }
-
-    public int getInt(String key, int defValue) {
-        return preferences.getInt(key, defValue);
-    }
-
-    public boolean putInt(String key, int value) {
-        return preferences.edit().putInt(key, value).commit();
-    }
-
-    public float getFloat(String key, float defValue) {
-        return preferences.getFloat(key, defValue);
-    }
-
-    public boolean putFloat(String key, float value) {
-        return preferences.edit().putFloat(key, value).commit();
-    }
-
-    public boolean clearData() {
-        return preferences.edit().clear().commit();
-    }
 }
